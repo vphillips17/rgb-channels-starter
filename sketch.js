2a371fa02b1cbd9638225f386676fea20fd240db
VictorP <vphillips@wlapcs.org>
2018-01-25 00:25:13 +0000

    done.

--- sketch.js
+++ sketch.js
@@ -2,15 +2,43 @@ var lpkidsred;
 var lpkidsgreen;
 var lpkidsblue;
 var lpkidsgray;
+var television
 
 function preload(){
-    
+    lpkidsgray = loadImage("images/lpkids-gray.png");
+    lpkidsred = loadImage("images/lpkids-red.png");
+    lpkidsgreen = loadImage("images/lpkids-green.png");
+    lpkidsblue = loadImage("images/lpkids-blue.png");
+    television = loadImage("images/OLD-SCHOOL-TV.png")
 }
 
 function setup(){
-    createCanvas(500,374);
+    createCanvas(749,524);
 }
 
 function draw(){
-    
+    if(keyIsPressed){
+        if(key=="r"){
+            image(lpkidsred, 0, 0);
+        }
+        if(key=="g"){
+            image(lpkidsgreen, 0, 0);
+        }
+        if(key=="b"){
+            image(lpkidsblue, 0, 0);
+        }
+        if(keyCode=="32"){
+            fill(0);
+            rect(0, 0, 500, 374);
+            blendMode(LIGHTEST);
+            image(lpkidsred,0,0);
+            image(lpkidsgreen,0,0);
+            image(lpkidsblue,0,0);
+        }
+    }
+    else{
+        blendMode(NORMAL);
+        image(lpkidsgray,50,50);
+         image(television, 0,0);
+    }
 }
